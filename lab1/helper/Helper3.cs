38744dde9e7e8615af6ddd0cd975e7f8942ec32e
Loki22978964 <lokim3059@gmail.com>
2026-03-24 09:39:50 +0200

    sad

---
+++ lab1/helper/Helper3.cs
@@ -0,0 +1,72 @@
+﻿using Data_Structures;
+using System.Collections.Generic;
+
+namespace helper
+{
+    public class helper3
+    {
+        public void Task3Simulation(Queue queue , LinkedList list)
+        {
+            Console.WriteLine("--- Starting Task 3 ---");
+            Console.WriteLine("Initial List content:");
+            list.Print();
+
+            Node current = list.Head;
+            List<int> positiveValues = new List<int>();
+
+            while (current != null)
+            {
+                if (current.value > 0)
+                {
+                    positiveValues.Add(current.value);
+                }
+                current = current.next;
+            }
+
+            foreach (int val in positiveValues)
+            {
+                list.Remove(val);
+
+                int lastInQueue = queue.IsEmpty() ? 0 : queue.GetLast();
+                int newValue = lastInQueue + val;
+
+                if (queue.Enqueue(newValue))
+                {
+                    Console.WriteLine($"Moved {val}: New queue element = {lastInQueue} + {val} = {newValue}");
+                }
+                else
+                {
+                    Console.WriteLine("Queue is full!");
+                    break;
+                }
+            }
+
+            Console.WriteLine("\nFinal state of List:");
+            list.Print();
+
+            Console.WriteLine("Final state of Queue:");
+            queue.Print();
+        }
+
+        public LinkedList GetLinkedList(int size)
+        {
+            Console.WriteLine($"--- Populating List (Size: {size}) ---");
+            LinkedList linkedList = new LinkedList();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter value for node {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int n))
+                {
+                    linkedList.Add(n);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Adding 0.");
+                    linkedList.Add(0);
+                }
+            }
+            return linkedList;
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using helper;
+using Data_Structures;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            helper1 queue = new helper1();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("First task");
+            queue.QueueSimulation();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("________________________________________________________________________________________________________________________");
+            
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Second task");
+
+            helper2 linkedList = new helper2();
+            linkedList.LinkedListSimulation();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("________________________________________________________________________________________________________________________");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Third task");
+            helper3 h3 = new helper3();
+
+            Console.Write("Enter how many elements you want in the initial list: ");
+            if (!int.TryParse(Console.ReadLine(), out int listSize)) listSize = 5;
+
+            LinkedList myList = h3.GetLinkedList(listSize);
+
+            Queue myQueue = new Queue(listSize);
+
+            h3.Task3Simulation(myQueue, myList);
+        }
+    }
+}
