0ecb3654ec7e48c61aab697708be842584fca968
Loki22978964 <lokim3059@gmail.com>
2026-04-27 11:41:24 +0300

    Asd

---
+++ lab6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using ConsoleApp1;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int N = 100;
+int[] sizes = { N, N * N, N * N * N }; // 100, 10,000, 1,000,000
+
+Console.WriteLine("Дослідження швидкодії (час у наносекундах):");
+Console.WriteLine("-----------------------------------------------------------------");
+Console.WriteLine("{0,10} | {1,20} | {2,20}", "Розмір", "Radix Binary", "Bubble Sort");
+Console.WriteLine("-----------------------------------------------------------------");
+
+foreach (int size in sizes)
+{
+    int[] originalData = GenerateData(size);
+
+    int[] dataForRadix = (int[])originalData.Clone();
+    long radixTime = MeasureNanoTime(() => SortingAlgorithms.RadixBinarySort(dataForRadix));
+
+    string bubbleResult;
+    if (size <= 10000)
+    {
+        int[] dataForBubble = (int[])originalData.Clone();
+        long bubbleTime = MeasureNanoTime(() => SortingAlgorithms.BubbleSort(dataForBubble));
+        bubbleResult = bubbleTime.ToString("N0");
+    }
+    else
+    {
+        bubbleResult = "Занадто довго (> O(N²))";
+    }
+
+    Console.WriteLine("{0,10} | {1,20:N0} | {2,20}", size, radixTime, bubbleResult);
+}
+
+Console.WriteLine("\n--- Аналіз для Завдання 3 (N=10,000) ---");
+RunAnalysis(10000);
+
+int[] GenerateData(int size)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++) arr[i] = rand.Next(0, int.MaxValue);
+    return arr;
+}
+
+long MeasureNanoTime(Action action)
+{
+    action();
+
+    Stopwatch sw = Stopwatch.StartNew();
+    action();
+    sw.Stop();
+
+    return (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+}
+
+void RunAnalysis(int size)
+{
+    int[] sorted = Enumerable.Range(0, size).ToArray();
+    int[] reverse = Enumerable.Range(0, size).Reverse().ToArray();
+    int[] random = GenerateData(size);
+
+    Console.WriteLine("{0,-15} | {1,15} | {2,15}", "Тип масиву", "Radix (нс)", "Bubble (нс)");
+    Console.WriteLine("-----------------------------------------------------------------");
+    Console.WriteLine("{0,-15} | {1,15:N0} | {2,15:N0}", "Відсортований",
+        MeasureNanoTime(() => SortingAlgorithms.RadixBinarySort(sorted)),
+        MeasureNanoTime(() => SortingAlgorithms.BubbleSort(sorted)));
+
+    Console.WriteLine("{0,-15} | {1,15:N0} | {2,15:N0}", "Зворотний",
+        MeasureNanoTime(() => SortingAlgorithms.RadixBinarySort(reverse)),
+        MeasureNanoTime(() => SortingAlgorithms.BubbleSort(reverse)));
+
+    Console.WriteLine("{0,-15} | {1,15:N0} | {2,15:N0}", "Випадковий",
+        MeasureNanoTime(() => SortingAlgorithms.RadixBinarySort(random)),
+        MeasureNanoTime(() => SortingAlgorithms.BubbleSort(random)));
+}
---
+++ lab6/ConsoleApp1/ConsoleApp1/SortingAlgorithms.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SortingAlgorithms
+    {
+        // Binary Radix Sort / MSB)
+        public static void RadixBinarySort(int[] array)
+        {
+            if (array == null || array.Length <= 1) return;
+            
+            SortBit(array, 0, array.Length - 1, 30);
+        }
+
+        private static void SortBit(int[] array, int left, int right, int bit)
+        {
+            if (left >= right || bit < 0) return;
+
+            int i = left;
+            int j = right;
+
+            while (i <= j)
+            {
+                while (i <= j && ((array[i] >> bit) & 1) == 0) i++;
+
+                while (i <= j && ((array[j] >> bit) & 1) == 1) j--;
+
+                if (i < j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            SortBit(array, left, j, bit - 1);
+            SortBit(array, i, right, bit - 1);
+        }
+
+        //Bubble Sort
+        public static void BubbleSort(int[] array)
+        {
+            if (array == null) return;
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                
+                if (!swapped) break;
+            }
+        }
+    }
+}
